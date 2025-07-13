f03c9155196be0ea00683ef736c915e7ab505365
sachin.gaikwad <sachindgaikwad0@gmail.com>
2025-07-13 10:31:38 +0530

    first commit

---
+++ src/test/java/com/codebook/ProductListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
